39875ca6746149378e95c6f6b276b44daa9e05fd
Rushabh Shah <rushabhshah@quarkstudios.com>
2021-02-17 19:33:59 +0530

    mongoose updated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,11 +5,20 @@ const {
 
 console.log('MONGO_URL',process.env.MONGO_URL)
 
-mongoose.connect(process.env.MONGO_URL + '/task-mager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
+// mongoose.connect(process.env.MONGO_URL + '/task-mager-api', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+// })
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://rushabh:<rushabh123>@cluster0.hhehm.mongodb.net/<task-mager-api>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 // const me = new User({
 //     name: 'Rushabh',
